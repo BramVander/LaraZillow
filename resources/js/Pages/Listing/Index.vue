0393f17c66beb01e35c84125af7de67f6da92245
Vander <bram1105@hotmail.com>
2023-01-11 12:59:46 +0100

    Listing Component

---
+++ resources/js/Pages/Listing/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div v-for="listing in listings" :key="listing.id">
+        <Link :href="`/listing/${listing.id}`">
+            <ListingAddress :listing="listing"></ListingAddress>
+        </Link>
+    </div>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+import ListingAddress from "../../components/ListingAddress.vue";
+
+defineProps({
+    listings: Array,
+});
+</script>
---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <ListingAddress :listing="listing"></ListingAddress>
+    </div>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/inertia-vue3";
+import ListingAddress from "../../components/ListingAddress.vue";
+
+defineProps({
+    listing: Object,
+});
+</script>
